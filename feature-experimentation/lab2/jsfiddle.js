ce6b2762a4082ba4c08e7d3f9b91f85e56117d3e
Mark Biesheuvel <mail@markbiesheuvel.nl>
2023-02-03 16:50:58 +0100

    Add Optimizely to lab2

--- feature-experimentation/lab2/jsfiddle.js
+++ feature-experimentation/lab2/jsfiddle.js
@@ -1,27 +1,46 @@
-const Product = ({title, image, description}) => {
+const userId = 'user123';
+const sdkKey = 'KVpGWnzPGKvvQ8yeEWmJZ';
+
+// Button component
+const Button = ({decision}) => {
+  return (
+    <a href="#" className={`btn btn-${decision.variationKey}`}>
+      Add to cart
+    </a>
+  );
+};
+
+// Product component
+const Product = ({decision, title, image, description}) => {
   return (
     <div className="card">
       <img className="card-img-top" src={image} alt={title}/>
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         <p className="card-text">{description}</p>
-        <a href="#" className="btn btn-primary">Add to cart</a>
+        <Button decision={decision}/>
       </div>
     </div>
   );
 };
 
+// App component
 const App = () => {
+	const user = optimizelyClient.createUserContext(userId);
+  const decision = user.decide('buy_button');
+
   return (
     <div className="row">
-      <div className="col-6">
+      <div className="col">
         <Product
+          decision={decision}
           title="Camel Marl Woven Scarf"
           image="https://cdn.shopify.com/s/files/1/0923/0916/products/TM56P13MCAM_Zoom_F_1_grande.jpg"
           description="100% Acrylic, Machine washable, Color: Brown, Code: 56P13MCAM"/>
       </div>
-      <div className="col-6">
+      <div className="col">
         <Product
+          decision={decision}
           title="SS Aloha Short Sleeve Buttondown"
           image="https://cdn.shopify.com/s/files/1/0923/0916/products/ZHURdWAGef_the_ss_aloha_popover_0_original_grande.jpg"
           description="The Summer Popover offers a one-two punch of personality: its slubby fabric offers great texture while the California-themed aloha print short sleeve style will turn every head on the boardwalk as you skate or surf by."/>
@@ -30,6 +49,20 @@ const App = () => {
   );
 };
 
+// Initialize Optimizely client
+const optimizelyClient = window.optimizelySdk.createInstance({
+  sdkKey,
+  datafileOptions: {
+    autoUpdate: true,
+    updateInterval: 5000,
+  },
+});
+
+// Initialize React root
 const container = document.getElementById('app');
 const root = ReactDOM.createRoot(container);
-root.render(<App/>);
+
+// Only start rendering once the Optimizely client is ready
+optimizelyClient.onReady().then(() => {
+	root.render(<App/>);
+});
